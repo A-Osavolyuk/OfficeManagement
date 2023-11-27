6cbd5097a381f88908e7e86297aca58b32874d25
A.Osavoliuk <sasha.osavoll11@gmail.com>
2023-11-27 16:31:27 +0200

    Added logging for DepartmentsAPI

--- DepartmentsAPI/Controllers/DepartmentsController.cs
+++ DepartmentsAPI/Controllers/DepartmentsController.cs
@@ -24,10 +24,17 @@ namespace DepartmentsAPI.Controllers
         {
             var result = await departmentsService.GetAll();
 
-            logger.LogInformation("Getting list of all departments");
             return result.Match<ActionResult<ResponseDto>>(
-                succ => Ok(new ResponseDto() { IsSucceeded = true, Result = succ}),
-                fail => StatusCode(500));
+                succ =>
+                {
+                    logger.LogInformation("Successful getting list of all departments.");
+                    return Ok(new ResponseDto() { IsSucceeded = true, Result = succ });
+                },
+                fail =>
+                {
+                    logger.LogWarning("Cannot get list of all departments. Status code 500.");
+                    return StatusCode(500);
+                });
         }
 
         [HttpGet("{id:int}")]
@@ -36,18 +43,34 @@ namespace DepartmentsAPI.Controllers
             var result = await departmentsService.GetById(id);
 
             return result.Match<ActionResult<ResponseDto>>(
-                succ => Ok(new ResponseDto() { IsSucceeded = true, Result = succ }),
-                fail => NotFound(fail.Message));
+                succ =>
+                {
+                    logger.LogInformation($"Successful getting department with id: {id}.");
+                    return Ok(new ResponseDto() { IsSucceeded = true, Result = succ });
+                },
+                fail =>
+                {
+                    logger.LogWarning(fail.Message);
+                    return NotFound(fail.Message);
+                });
         }
 
         [HttpPost]
-        public async ValueTask<ActionResult<ResponseDto>> CreateDepartmnet([FromBody]DepartmentDto department)
+        public async ValueTask<ActionResult<ResponseDto>> CreateDepartment([FromBody]DepartmentDto department)
         {
             var result = await departmentsService.Create(department);
 
             return result.Match<ActionResult<ResponseDto>>(
-                succ => Ok(new ResponseDto() { IsSucceeded = true, Result = succ }),
-                fail => BadRequest(fail.Message));
+                succ =>
+                {
+                    logger.LogInformation($"Department with name: {succ.DepartmentName} was create successful.");
+                    return Ok(new ResponseDto() { IsSucceeded = true, Result = succ });
+                },
+                fail =>
+                {
+                    logger.LogWarning(fail.Message);
+                    return BadRequest(fail.Message);
+                });
         }
 
         [HttpDelete("{id:int}")]
@@ -56,21 +79,37 @@ namespace DepartmentsAPI.Controllers
             var result = await departmentsService.DeleteById(id);
 
             return result.Match<ActionResult<ResponseDto>>(
-                succ => Ok(new ResponseDto() { IsSucceeded = true, Message = $"Department with id: {id} was deleted successful." }),
-                fail => NotFound(fail.Message));
+                succ =>
+                {
+                    logger.LogInformation($"Department with id: {id} was deleted successful.");
+                    return Ok(new ResponseDto() { IsSucceeded = true, Message = $"Department with id: {id} was deleted successful." });
+                },
+                fail => { 
+                    logger.LogWarning(fail.Message); 
+                    return NotFound(fail.Message); 
+                });
         }
 
         [HttpPut("{id:int}")]
-        public async ValueTask<ActionResult<ResponseDto>> CreateDepartment([FromBody] DepartmentDto department, int id)
+        public async ValueTask<ActionResult<ResponseDto>> UpdateDepartment([FromBody] DepartmentDto department, int id)
         {
             var result = await departmentsService.Update(department, id);
 
             return result.Match<ActionResult<ResponseDto>>(
-                succ => Ok(new ResponseDto() { IsSucceeded = true, Result = succ }),
+                succ =>
+                {
+                    logger.LogInformation($"Department with id: {succ.DepartmentId} was updated successful");
+                    return Ok(new ResponseDto() { IsSucceeded = true, Result = succ });
+                },
                 fail =>
                 {
-                    if (fail is ValidationException exception) 
+                    if (fail is ValidationException exception)
+                    {
+                        logger.LogWarning(exception.Message);
                         return BadRequest(exception.Message);
+                    }
+
+                    logger.LogWarning(fail.Message);
                     return NotFound(fail.Message);
                 });
         }
