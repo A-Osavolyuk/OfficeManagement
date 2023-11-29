9a71ff9946ad19360f646290a0a958ba60aee9fe
A.Osavoliuk <sasha.osavoll11@gmail.com>
2023-11-29 11:36:43 +0200

    Added AuthController and main concept of JWT authentication and authorization

---
+++ AuthAPI/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using AuthAPI.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public async ValueTask<ActionResult<ResponseDto>> Login()
+        {
+            return Ok();
+        }
+
+        public async ValueTask<ActionResult<ResponseDto>> Register()
+        {
+            return Ok();
+        }
+
+        public async ValueTask<ActionResult<ResponseDto>> AssignRole()
+        {
+            return Ok();
+        }
+
+        public async ValueTask<ActionResult<ResponseDto>> ChangePassword()
+        {
+            return Ok();
+        }
+    }
+}
