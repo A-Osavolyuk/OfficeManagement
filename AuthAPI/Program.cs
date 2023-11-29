093525b67b29f136139e13f0dcea514939498c12
A.Osavoliuk <sasha.osavoll11@gmail.com>
2023-11-29 11:40:53 +0200

    Added logging support

--- AuthAPI/Program.cs
+++ AuthAPI/Program.cs
@@ -3,6 +3,10 @@ using AuthAPI.Models.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog.Sinks.Elasticsearch;
+using Serilog;
+using System.Reflection;
+using Serilog.Exceptions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +22,9 @@ builder.Services.AddDbContextFactory<DataContext>(o => o.UseSqlServer(builder.Co
 builder.Services.AddIdentity<AppUser, IdentityRole>()
     .AddEntityFrameworkStores<DataContext>()
     .AddDefaultTokenProviders();
+
+ConfigureLogging();
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
@@ -35,3 +42,34 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void ConfigureLogging()
+{
+    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+    var configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{environment}.json", optional: true)
+        .Build();
+
+    Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .Enrich.WithExceptionDetails()
+        .WriteTo.Debug()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+        .Enrich.WithProperty("Environment", environment)
+        .ReadFrom.Configuration(configuration)
+        .CreateLogger();
+}
+
+ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string? environment)
+{
+    return new ElasticsearchSinkOptions(new Uri(configuration["ElasticSearchConfiguration:Uri"]))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment.ToLower()}-{DateTime.UtcNow:yyyy-mm}",
+        NumberOfReplicas = 1,
+        NumberOfShards = 2,
+    };
+}
